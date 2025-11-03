cc0ce789ca3ada7508a0a480cd6f0de9d46428e8
mohammedismail1 <mohammedismail@talrop.com>
2025-11-03 14:14:16 +0530

    fix(navigation): fix issue

--- src/data/TabNavigationMenus.jsx
+++ src/data/TabNavigationMenus.jsx
@@ -11,30 +11,50 @@ export const tabNavigationMenus = [
     id: 2,
     label: "Sessions",
     value: "sessions",
-    content: <div>Sessions content goes here.</div>,
+    content: (
+      <div className="flex items-center justify-center h-[calc(100vh-140px)]  ">
+        Sessions content goes here.
+      </div>
+    ),
   },
   {
     id: 3,
     label: "Notifications",
     value: "notifications",
-    content: <div>Notifications content goes here.</div>,
+    content: (
+      <div className="flex items-center justify-center h-[calc(100vh-140px)]  ">
+        Notifications content goes here.
+      </div>
+    ),
   },
   {
     id: 4,
     label: "Media",
     value: "media",
-    content: <div>Media content goes here.</div>,
+    content: (
+      <div className="flex items-center justify-center h-[calc(100vh-140px)]  ">
+        Media content goes here.
+      </div>
+    ),
   },
   {
     id: 5,
     label: "Whiteboard",
     value: "whiteboard",
-    content: <div>Whiteboard settings go here.</div>,
+    content: (
+      <div className="flex items-center justify-center h-[calc(100vh-140px)]  ">
+        Whiteboard settings go here.
+      </div>
+    ),
   },
   {
     id: 6,
     label: "Account",
     value: "account",
-    content: <div>Account settings go here.</div>,
+    content: (
+      <div className="flex items-center justify-center h-[calc(100vh-140px)]  ">
+        Account settings go here.
+      </div>
+    ),
   },
 ];
