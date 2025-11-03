68700fb95b416d3f8f6fedd4e5da6e5c6dff13b1
mohammedismail1 <mohammedismail@talrop.com>
2025-11-03 19:04:24 +0530

    fix(issues): fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/tutor-app/",
+  // base: "/tutor-app/",
 });
